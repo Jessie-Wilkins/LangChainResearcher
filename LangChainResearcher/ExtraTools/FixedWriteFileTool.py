576db05f96d074e066412e27f67f592f3ca53d18
Jessie Wilkins <jessiekwilkins.yahoo.com@gmail.com>
2023-05-10 18:44:04 -0500

    Updated Fixed write tool to write a list with a pydantic object

--- LangChainResearcher/ExtraTools/FixedWriteFileTool.py
+++ LangChainResearcher/ExtraTools/FixedWriteFileTool.py
@@ -7,37 +7,21 @@ import json
 from json import JSONDecodeError
 
 class FixedWriteFileTool(WriteFileTool):
-    description = (
-        "Write file to disk." 
-        "Takes the file path and the text content as two string arguments."
-        "An example of this input: \"file_path\": \"test.txt\", \"text\": \"This is some test text.\""
-        "Follow this example strictly; make sure to always use the double quotes just like in the example when working with the real inputs."
-        "If a user asks for a specific format, change the text value to follow that format."
-    )
     args_schema: Optional[Type[BaseModel]] = None
 
-    @staticmethod
-    def is_valid_json(input_string):
-        try:
-            json.loads(input_string)
-            return True
-        except JSONDecodeError:
-            return False
-
-    def convert_list_to_text(file_path: str, list: List[Item]):
-        return ""
+    def convert_list_to_text(self, list: List[Item]):
+        formatted_list = ""
+        count = 1
+        for item in list:
+            print(item)
+            if count >= 2:
+                formatted_list=formatted_list+"\n"
+            formatted_list=formatted_list+f"{count}. {item.item}: {item.description}"
+            count=count+1
+        return formatted_list
 
         
 
-    def _run(self, file_path_and_text: str) -> str:
-        print()
-        print(file_path_and_text)
-        file_path_and_text = "{\"" + file_path_and_text + "\"}"
-        print()
-        print(file_path_and_text)
-        file_path_and_text_json = None
-        if FixedWriteFileTool.is_valid_json(file_path_and_text):
-            file_path_and_text_json = json.loads(file_path_and_text)
-            return super()._run(file_path_and_text_json["file_path"], file_path_and_text_json["text"])
-        else:
-            return "Failed to write file."
+    def _run(self, itemList: ItemList) -> str:
+        item_list_string=self.convert_list_to_text(itemList.list)
+        return super()._run(itemList.file_path, item_list_string)
--- test/ExtraTools/FixedWriteFileTool_test.py
+++ test/ExtraTools/FixedWriteFileTool_test.py
@@ -1,19 +1,43 @@
 import pytest
+import os
 from LangChainResearcher.ExtraTools.FixedWriteFileTool import FixedWriteFileTool
 from LangChainResearcher.ResearchAgentImpl.ResearchAgentOutputModels import Item, ItemList
 
+
 def test_convert_json_to_text_for_list():
 
-        item1 = Item(item="Chicken Eggs", description="Eggs that come from chickens")
-        item2 = Item(item="Duck Eggs", description="Eggs that come from ducks")
-        item3 = Item(item="Robin Eggs", description="Eggs that come from robins")
+    item1 = Item(item="Chicken Eggs",
+                 description="Eggs that come from chickens")
+    item2 = Item(item="Duck Eggs", description="Eggs that come from ducks")
+    item3 = Item(item="Robin Eggs", description="Eggs that come from robins")
 
-        itemList = ItemList(file_path="output/eggs.txt", list=[item1, item2, item3])
+    itemList = ItemList(file_path="output/eggs.txt",
+                        list=[item1, item2, item3])
 
-        writer = FixedWriteFileTool()
+    writer = FixedWriteFileTool()
 
-        itemListString = """1. Chicken Eggs: Eggs that come from chickens\n
-        2. Duck Eggs: Eggs that come from ducks\n
-        3. Robin Eggs: Eggs that come from robins"""
+    itemListString = "1. Chicken Eggs: Eggs that come from chickens\n2. Duck Eggs: Eggs that come from ducks\n3. Robin Eggs: Eggs that come from robins"
 
-        assert writer.convert_list_to_text(itemList) == itemListString
+    assert writer.convert_list_to_text(itemList.list) == itemListString
+
+
+def test_run_can_create_a_file_in_the_path():
+    writer = FixedWriteFileTool()
+    item1 = Item(item="Chicken Eggs",
+                 description="Eggs that come from chickens")
+    item2 = Item(item="Duck Eggs", description="Eggs that come from ducks")
+    item3 = Item(item="Robin Eggs", description="Eggs that come from robins")
+
+    itemList = ItemList(file_path="output/eggs.txt",
+                        list=[item1, item2, item3])
+    writer._run(itemList)
+
+    assert os.path.isfile(itemList.file_path)   
+
+    with open(itemList.file_path, 'r') as f:
+        contents = f.read()
+    expected_contents = writer.convert_list_to_text(itemList.list)
+    assert contents == expected_contents
+
+    os.remove(itemList.file_path)
+
