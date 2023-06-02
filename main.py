6dddc6b1633789ac833c21fda0ad3babfae45ddf
Jessie Wilkins <jessiekwilkins.yahoo.com@gmail.com>
2023-06-01 21:39:34 -0500

    Changing model to gpt3.5

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from langchain import OpenAI
+from langchain.chat_models import ChatOpenAI
 from LangChainResearcher.ResearchAgentImpl.ResearchAgentPrompt import Format
 from LangChainResearcher.ResearchAgentImpl.ResearchAgentImpl import ResearchAgentImpl
 
 def main():
-    llm = OpenAI(temperature=0)
+    llm = ChatOpenAI(temperature=0)
     ResearchAgentImpl.Run(llm, "What are the latest open source LLMs.", Format.LIST)
 
 if __name__ == "__main__":
